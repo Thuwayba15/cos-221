c52f4e12fa536c721ed3e3dbb90194de09b32ba3
s-sacoor8 <116795587+s-sacoor8@users.noreply.github.com>
2024-05-29 21:57:28 +0200

    Add files via upload

---
+++ movies.php
@@ -0,0 +1,92 @@
+<?php include 'header.php'; ?>
+
+
+
+
+</header>
+
+
+<main>
+    <link rel="stylesheet" href="css/movies.css">
+    <section id="movies">
+        <div class="movie-container">
+
+
+            <div class="movie">
+                <img src="img/movie1.jpeg" alt="Movie 1">
+                <p>Fast and Furious 1</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="1">View More</button>
+                        <button class="watchlist-button" data-movie-id="1">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+            </div>
+
+
+            <div class="movie">
+                <img src="img/movie2.jpg" alt="Movie 2">
+                <p>Fast and Furious 2</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="2">View More</button>
+                        <button class="watchlist-button" data-movie-id="2">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+
+            </div>
+
+
+            <div class="movie">
+                <img src="img/movie3.jpeg" alt="Movie 3">
+                <p>Fast and Furious 3</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="3">View More</button>
+                        <button class="watchlist-button" data-movie-id="3">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+            </div>
+
+            
+            <div class="movie">
+                <img src="img/movie4.jpg" alt="Movie 4">
+                <p>Fast and Furious 4</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="4">View More</button>
+                        <button class="watchlist-button" data-movie-id="4">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+            </div>
+
+
+            <div class="movie">
+                <img src="img/movie5.jpg" alt="Movie 5">
+                <p>Fast and Furious 5</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="5">View More</button>
+                        <button class="watchlist-button" data-movie-id="5">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+            </div>
+
+
+            <div class="movie">
+                <img src="img/movie6.jpg" alt="Movie 6">
+                <p>Fast and Furious 6</p>
+                <div class="button-container">
+                    <button class="view-more-button" data-movie-id="6">View More</button>
+                        <button class="watchlist-button" data-movie-id="6">Add to Watchlist</button>
+                        <button class="share-button">Share</button>
+                        <button class="like-button" data-movie-id="1">&#x2661;</button>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+     <script src="js/movies.js"></script>
+</main>
+
+<?php include 'footer.php'; ?>
---
+++ profile.php
@@ -0,0 +1,31 @@
+<?php include 'header.php'; ?>
+
+</header>
+<main>
+    <link rel="stylesheet" href="css/profile.css">
+    <section id="profile">
+        <h1>MY PROFILE</h1>
+        <div class="profile-info">
+            <div class="profile-item">
+                <label for="username">Username</label>
+                <input type="text" id="username" readonly>
+            </div>
+            <div class="profile-item">
+                <label for="email">Email</label>
+                <input type="email" id="email" readonly>
+            </div>
+            <div class="profile-item">
+                <label for="subscription">Subscription Plan</label>
+                <input type="text" id="subscription" readonly>
+            </div>
+            <div class="profile-item">
+                <label for="account-type">Account Type</label>
+                <input type="text" id="account-type" readonly>
+            </div>
+        </div>
+        <button id="logout-button">Log out</button>
+    </section>
+    <script src="js/profile.js"></script>
+</main>
+
+<?php include 'footer.php'; ?>
